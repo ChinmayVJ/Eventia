6ec3e3d2197119a1a82a983a1116bb0b606a9a92
ChinmayVJ <chinmayj1505@gmail.com>
2019-09-14 12:08:27 +0530

    Login, Sign Up and Home Page Layout

---
+++ app/src/main/java/com/example/loginactivity/HomePage.java
@@ -0,0 +1,52 @@
+package com.example.loginactivity;
+
+import android.os.Bundle;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+
+import android.view.MenuItem;
+import android.widget.TextView;
+
+public class HomePage extends AppCompatActivity {
+    private TextView mTextMessage;
+
+    private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
+            = new BottomNavigationView.OnNavigationItemSelectedListener() {
+
+        @Override
+        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+            Fragment selectedFragment = null;
+            switch (item.getItemId()){
+                case R.id.navigation_home:
+                    selectedFragment = new FragmentHome();
+                    break;
+                case R.id.navigation_dashboard:
+                    selectedFragment = new FragmentExplore();
+                    break;
+                case R.id.navigation_notifications:
+                    selectedFragment = new FragmentNotifications();
+                    break;
+            }
+
+            getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, selectedFragment).commit();
+            return true;
+        }
+    };
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home_page);
+        BottomNavigationView navView = findViewById(R.id.nav_view);
+        mTextMessage = findViewById(R.id.message);
+        navView.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+
+        getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new FragmentHome()).commit();
+
+    }
+
+}
--- app/src/main/java/com/example/loginactivity/MainActivity.java
+++ app/src/main/java/com/example/loginactivity/MainActivity.java
@@ -5,23 +5,33 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
     TextView loginText;
+    Button loginButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        loginButton = findViewById(R.id.logIn_button);
         loginText = findViewById(R.id.logIn_text);
 
         loginText.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 startActivity(new Intent(getApplicationContext(), RegistrationActivity.class));
+            }
+        });
+
+        loginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(getApplicationContext(), HomePage.class));
             }
         });
     }
