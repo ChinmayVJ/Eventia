f1c645233efa45586a0f07f33220fbe853ac9645
ChinmayVJ <chinmayj1505@gmail.com>
2019-09-16 00:29:44 +0530

    Changes made in Layout

--- app/src/main/java/com/example/loginactivity/CreateEvent.java
+++ app/src/main/java/com/example/loginactivity/CreateEvent.java
@@ -6,16 +6,20 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageButton;
+import android.widget.ImageView;
 
 public class CreateEvent extends AppCompatActivity {
 
     ImageButton backButton;
+    ImageView companyPic;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_event);
 
         backButton = findViewById(R.id.toolbar_back_button);
+        companyPic = findViewById(R.id.company_pic);
 
         backButton.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -23,5 +27,12 @@ public class CreateEvent extends AppCompatActivity {
                 startActivity(new Intent(getApplicationContext(), HomePage.class));
             }
         });
+
+        /*companyPic.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(getApplicationContext(), EventInfo.class));
+            }
+        });*/
     }
 }
