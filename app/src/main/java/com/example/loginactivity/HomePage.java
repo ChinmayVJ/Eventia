7d78a23eb4530a92cc6c1476ff815e6c9e61aedb
ChinmayVJ <chinmayj1505@gmail.com>
2019-09-15 12:13:07 +0530

    Home Activity modified

--- app/src/main/java/com/example/loginactivity/HomePage.java
+++ app/src/main/java/com/example/loginactivity/HomePage.java
@@ -6,13 +6,31 @@ import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.annotation.NonNull;
+import androidx.appcompat.widget.Toolbar;
 import androidx.fragment.app.Fragment;
 
+import android.text.Layout;
 import android.view.MenuItem;
 import android.widget.TextView;
 
 public class HomePage extends AppCompatActivity {
-    private TextView mTextMessage;
+
+    Toolbar homeToolbar;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home_page);
+        BottomNavigationView navView = findViewById(R.id.nav_view);
+        navView.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+
+        homeToolbar = findViewById(R.id.toolbar_home);
+        setSupportActionBar(homeToolbar);
+        getSupportActionBar().setTitle("MeetUp");
+
+        getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new FragmentHome()).commit();
+
+    }
 
     private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
             = new BottomNavigationView.OnNavigationItemSelectedListener() {
@@ -36,17 +54,5 @@ public class HomePage extends AppCompatActivity {
             return true;
         }
     };
-
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_home_page);
-        BottomNavigationView navView = findViewById(R.id.nav_view);
-        mTextMessage = findViewById(R.id.message);
-        navView.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
-
-        getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new FragmentHome()).commit();
-
-    }
 
 }
--- app/src/main/java/com/example/loginactivity/MainActivity.java
+++ app/src/main/java/com/example/loginactivity/MainActivity.java
@@ -31,7 +31,7 @@ public class MainActivity extends AppCompatActivity {
         loginButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                startActivity(new Intent(getApplicationContext(), HomePage.class));
+                startActivity(new Intent(getApplicationContext(), EventInfo.class));
             }
         });
     }
